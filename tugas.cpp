0f1790d55dfc27cc028ec9401f286e5285d0a30f
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-01 08:03:14 +0700

    Membuat Prosedut Input Data bertipe Publik

--- tugas.cpp
+++ tugas.cpp
@@ -19,4 +19,87 @@ class Segitiga{
     bool HitungLuasTerhitung, HitungKelilingTerhitung;
    
     Segitiga () : HitungLuasTerhitung(false), HitungKelilingTerhitung(false){}
+
+    void inputData()
+    {
+        do
+        {
+            cout << "==============Menu==============" << endl;
+            cout << "1. Hitung Luas Segitiga" << endl;
+            cout << "2. Hitung Keliling Segitiga" << endl;
+            cout << "3. keluar" << endl;
+            cout << "Masukkan Pilihan : " << endl;
+            cin >> pilihan;
+    
+            switch (pilihan)
+            {
+            case 1 :
+                cout << "Menghitung Luas Segitiga" << endl;
+
+            while (true)
+            {
+                cout << "Masukkan Alas : ";
+                cin >> alassg;
+
+                if (alassg % 2 == 0 && alassg >= 2 && alassg <= 50)
+                {
+                    break;
+                }
+                else
+                {
+                    cout << "Alas yang dimasukkan adalah angka ganjil. Luas tidak dapat dihitung!"<<endl;
+                }
+            }
+
+            while (true)
+            {
+                cout << "Masukkan Tinggi : ";
+                cin >> tinggisg;
+
+                if (tinggisg % 2 == 0 && tinggisg >= 2 && tinggisg <= 50)
+                {
+                    HitungLuasTerhitung = true;
+                    break;
+                }
+                else
+                {
+                    cout << "Tinggi yang dimasukkan adalah angka ganjil. Luas tidak dapat dihitung!"<<endl;
+                }
+            }
+
+                cout << "Luas Segitiga = "
+                << HitungLuasSegitiga(alassg, tinggisg) << endl;
+                break;
+            case 2 :
+                cout << "Menghitung Keliling Segitiga" << endl;
+            
+            while (true)
+            {
+                cout << "Masukkan Panjang Sisi = ";
+                cin >> sisisg;
+                if (sisisg % 2 == 0 && sisisg >= 2 && sisisg <= 50)
+                {
+                    HitungKelilingTerhitung = true;
+                    break;
+                }
+                else
+                {
+                    cout << "Sisi yang dimasukkan adalah angka ganjil. Luas tidak dapat dihitung!"<<endl;
+                }
+            }
+
+                cout << "Keliling Segitiga = "
+                << HitungKelilingSegitiga(sisisg) << endl;
+                break;
+            case 3 :
+                break;
+            default:
+                cout << "Pilihan anda salah" << endl;
+                break;
+            }
+    
+        } while (pilihan != 3);
+    
+    }
+
 };
